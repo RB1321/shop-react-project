09690b22a4aa807d98f85ff04a81717b558bb832
Denys <denus777@gmail.com>
2023-03-01 18:55:51 +0200

    first html structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-let a = 10
+// const title = React.createElement('h1', { id: 'title' }, 'Hello React.js')
+// console.log(title)
+//jsx - js extended
+
+const title = <h1 id="title">Hi React.js</h1>
+const content = (
+    <div>
+        {title}
+        {10 + 10}
+        <ul className="list">
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li> List item 3</li>
+        </ul>
+        <p>Hello</p>
+    </div>
+)
+console.log(content)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode> </React.StrictMode>)
+root.render(<React.StrictMode> {content}</React.StrictMode>)
