e561a9e040db0e3232a1f58b515a19c95043378b
Denys <denus777@gmail.com>
2023-03-05 17:17:14 +0200

    css with classes module

--- src/List.tsx
+++ src/List.tsx
@@ -1,4 +1,7 @@
+import classes from './List.module.css'
 type Props = {}
+console.log(classes)
+const { content, test, articleDescription } = classes
 export const List = (props: Props) => {
     let data = false
     let styleObj = {
@@ -13,6 +16,12 @@ export const List = (props: Props) => {
                 <li>2222</li>
                 <li>3333333</li>
             </ul>
+            <p className={`${articleDescription} ${data ? content : test}`}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Asperiores eligendi ut error rerum ipsam. Tempora aspernatur
+                officia, sequi, et minima excepturi quas dicta harum, beatae
+                fugit animi aperiam itaque nobis?
+            </p>
             <p style={styleObj}>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Asperiores eligendi ut error rerum ipsam. Tempora aspernatur
