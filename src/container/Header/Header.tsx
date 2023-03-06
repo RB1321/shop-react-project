14fb51ddb4c03014e56381e6f92ebbb574c27b50
Denys <denus777@gmail.com>
2023-03-06 18:54:01 +0200

    add component Menu

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,12 +1,11 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import './Header.scss'
 import Logo from 'components/Logo/Logo'
+import Menu from 'components/Menu/Menu'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -25,7 +24,7 @@ const Header = (props: Props) => {
                             <MenuIcon />
                         </IconButton>
                         <Logo />
-                        <Button color="inherit">Login</Button>
+                        <Menu />
                     </Toolbar>
                 </Container>
             </AppBar>
