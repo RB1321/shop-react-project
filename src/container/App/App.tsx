2d17c1b9ae41ffbe81fd3346d9cee914331be64e
Denys <denus777@gmail.com>
2023-03-06 17:09:40 +0200

    add appbar in header

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,12 +1,14 @@
-import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
+import Footer from 'container/Footer/Footer'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
         <div>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
